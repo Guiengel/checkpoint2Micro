d91dacf70910bd0a4fa6dae0571ec8a1098cc0f5
Guilherme <guilhermengel1@gmail.com>
2025-05-04 21:45:31 -0300

    Commitando a continuação do projeto

--- src/main/java/br/com/fiap/checkpoint1/model/Paciente.java
+++ src/main/java/br/com/fiap/checkpoint1/model/Paciente.java
@@ -1,11 +1,61 @@
 package br.com.fiap.checkpoint1.model;
 
+
 public class Paciente {
 
+    private Long id;
     private String nome;
     private String endereco;
     private String bairro;
     private String email;
     private String telefone_completo;
+
+    public Long getId(){
+        return id;
+    }
+    
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+    
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getEndereco(){
+        return endereco;
+    }
+    
+    public void setEndereco(String endereco){
+        this.endereco = endereco;
+    }
+
+    public String getBairro(){
+        return bairro;
+    }
+    
+    public void setBairro(String bairro){
+        this.bairro = bairro;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+    
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getTelefone_Completo(){
+        return telefone_completo;
+    }
+    
+    public void setTelefone_Completo(String telefone_completo){
+        this.telefone_completo = telefone_completo;
+    }
 
 }
